ac080e0a817a16d0d28b0732bcfadb7dade5893c
Pavel Bekirov <tdostupa@gmail.com>
2023-03-05 23:16:06 +0400

    add vebinars

---
+++ vebinars/automatisation/Telegram Logs.py
@@ -0,0 +1,17 @@
+from notifiers import get_notifier
+def make_telegram_notifier(
+        token='6172070577:AAGERfg9Cqv2vF6UN_NDs2AY9-mGsjR3e8U',
+        chat_id=462441070):
+    def f(text):
+        telegram = get_notifier('telegram')
+        telegram.notify(message=text,
+                        token=token,
+                        chat_id=chat_id)
+    return f
+
+send = make_telegram_notifier()
+send("Hou are you?")
+
+
+send('Super')#%%
+#%%
